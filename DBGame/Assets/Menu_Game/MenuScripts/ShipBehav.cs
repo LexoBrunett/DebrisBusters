1f9000518686d7cea80e1c2baa744e069f99d124
LexoBrunett <56777101+LexoBrunett@users.noreply.github.com>
2020-10-04 16:46:08 -0500

    implementacion de cosas en scena de juego

---
+++ DBGame/Assets/Menu_Game/MenuScripts/ShipBehav.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(RigidBody2D))]
+public class ShipBehav : MonoBehaviour
+{
+    RigidBody2D rb;
+    public float Velocidad = 8f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb= GetComponent<RigidBody2D> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = Velocidad * (Input.GetAxis("Horizontal") * Vector3.right + Input.GetAxis("Vertical") * Vector3.foward)
+    }
+}
