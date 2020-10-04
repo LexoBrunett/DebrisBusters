e61ac2675f54fdb1436707b7a8eb87d4b6e6b3bb
LexoBrunett <56777101+LexoBrunett@users.noreply.github.com>
2020-10-04 15:14:43 -0500

    Levels Located

--- DBGame/Assets/Menu_Game/MenuScripts/EarthFunction.cs
+++ DBGame/Assets/Menu_Game/MenuScripts/EarthFunction.cs
@@ -1,25 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
+namespace SpeedTutorMainMenuSystem
+{
 public class EarthFunction : MonoBehaviour
 {
     // Start is called before the first frame update
+    string scena1;
+    string scena2;
+
     void Start()
     {
         
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    
-    public void startLevel()
-    {
-        Debug.Log("Next scene");
-    }
-    
-}
+    public void ResetButton(string ButtonType)
+            {
+            if (ButtonType == "Mission3")
+            {
+                SceneManager.LoadScene(scena1);
 
+            }
+
+            if (ButtonType == "Back")
+            {
+                SceneManager.LoadScene(scena2);
+
+            }           
+            }
+
+}
+}
