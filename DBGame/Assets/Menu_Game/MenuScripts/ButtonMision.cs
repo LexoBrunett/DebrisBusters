cddaa3814626d8757f689b4c46a9cd6a82234292
AndreaSoriano <aasorian@espol.edu.ec>
2020-10-04 23:44:16 -0500

    Escena puzzle agregada

---
+++ DBGame/Assets/Menu_Game/MenuScripts/ButtonMision.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonMision : MonoBehaviour
+{
+    public void cambiarEscena(string nombreEscena){
+        SceneManager.LoadScene(3);
+    }
+}
